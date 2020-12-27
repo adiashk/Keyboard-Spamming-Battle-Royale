9fe254bc7895c73a09029037348f8dba0364e0b6
adiashk <ashkenazi94@gmail.com>
2020-12-27 17:09:44 +0200

    init1

--- TCPClient.py
+++ TCPClient.py
@@ -1,10 +1,11 @@
 from socket import *
-serverName = "servername"
+serverName = ""
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 sentence = input("Input lowercase sentence:")
-clientSocket.send(sentence)
+clientSocket.send(sentence.encode('utf-8'))
+# clientSocket.sendto(sentence,(serverName, serverPort))
 modifiedSentence = clientSocket.recv(1024)
 print ("From Server:", modifiedSentence)
 clientSocket.close()
