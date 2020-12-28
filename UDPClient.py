01450cc354c5311350c28a995307f2141deadd48
adiashk <ashkenazi94@gmail.com>
2020-12-28 20:55:54 +0200

    start broadcast

--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,9 @@
 from socket import *
 serverName = 'hostname'
-serverPort = 12000
+serverPort = 12004
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-# message = input("Input lowercase sentence:")
-message = "abcd"
-clientSocket.sendto(message,(serverName, serverPort))
+message = input("Input lowercase sentence:")
+clientSocket.sendto(message.encode('utf-8'),('', serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print (modifiedMessage)
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,8 +1,9 @@
 from socket import *
-serverPort = 12000
+serverPort = 12004
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-print ("The server is ready to receive")
+print("Server started, listening on IP address 172.1.0.4")
+# “offer” announcements via UDP broadcast once every second
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print (message)
