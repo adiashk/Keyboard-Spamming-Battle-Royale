7ddf0ba7bbe20594390b581a9a7eec042fcd1a13
adiashk <ashkenazi94>
2020-12-29 12:55:42 +0200

    start game

--- Client.py
+++ Client.py
@@ -1,7 +1,9 @@
 from socket import *
-print ("Client started, listening for offer requests...")
-serverPort = 13117
-while True:
+team_name = "yuval_adi"
+
+def UDP_connection():    
+    print ("Client started, listening for offer requests...")
+    serverPort = 13117
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
@@ -9,9 +11,22 @@ while True:
     message, serverAddress = clientSocket.recvfrom(2048)
     print("message= ",message)
     print("serverAddress= ", serverAddress)
-    print("----------")
     clientSocket.close()
-    
+    return message, serverAddress
+
+def TCP_connection(message, serverAddress, team_name):
     print("Received offer from ", serverAddress, " attempting to connect...")
     serverPort = message[13:].decode('utf-8')
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(('', serverPort))
+    clientSocket.send(team_name.encode('utf-8'))
+    modifiedSentence = clientSocket.recv(1024)
+
+    
+    clientSocket.close()
     # GAME
+
+
+while True:
+    message, serverAddress = UDP_connection()
+    TCP_connection(message, serverAddress, team_name)
--- Server.py
+++ Server.py
@@ -1,19 +1,66 @@
 from socket import *
 import threading
 import time
+from collections import defaultdict 
+
+serverPort = 13117
+serverSocket_UDP = socket(AF_INET, SOCK_DGRAM)
+serverSocket_UDP.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket_UDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+serverSocket_TCP = socket(AF_INET,SOCK_STREAM)
+serverSocket_TCP.bind(('',serverPort))
+serverSocket_TCP.listen(1)
+
+clients_group1 = defaultdict(int)
+clients_group2 = defaultdict(int)
+groups_counter = 0
 
 def start_server():
-    print( "Server started, listening on IP address 172.1.0.4")
-    print_offers()
+    print("Server started, listening on IP address 172.1.0.4")
+    start_time = time.time()
+    while time.time() - start_time < 10:
+        offer_UDP_connection()
+    game()
 
-def print_offers():
-    serverPort = 13117
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
-    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+def offer_UDP_connection():
     message = "0xfeedbeef" + "0x2" + "12000"
     threading.Timer(1.0, print_offers).start()
-    serverSocket.sendto(message.encode('utf-8'), ('', serverPort))
+    serverSocket_UDP.sendto(message.encode('utf-8'), ('', serverPort))
+    TCP_connection()
+
+
+def TCP_connection():
+    connectionSocket, addr = serverSocket_TCP.accept()
+    team_name = connectionSocket.recv(1024)
+    groups_counter += 1
+    if groups_counter % 2 == 1:
+        clients_group1[team_name] = 0
+    elif
+        clients_group2[team_name] = 0
+
+def game():
+    print_game_start()
+    message = "Start pressing keys on your keyboard as fast as you can!!"
+    
+
+def print_game_start():
+    print("Welcome to Keyboard Spamming Battle Royale.")
+    print("Group 1:")
+    print("==")
+    for name in clients_group1.keys():
+        print(name)
+    print("Group 2:")
+    print("==")
+    for name in clients_group2.keys():
+        print(name)
+    
 
 
 start_server()
+
+
+# magic_cookie = 0xfeedbeef
+# message_type= 0x2
+# message = struct.pack('bbb', magic_cookie, message_type,localPort)
